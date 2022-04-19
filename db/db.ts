f1ac9514164fc4f11cdc7bf1097282a83ef4cdf9
DawsonCodes <101001810+Dawsoncodes@users.noreply.github.com>
2022-04-19 07:53:36 +0300

    .env

--- db/db.ts
+++ db/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDb = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/cheatsheets")
+    await mongoose.connect(process.env.MONGO_URI as string)
     console.log("MongoDB connected")
   } catch (error) {
     if (error instanceof Error) console.log(error.message)
